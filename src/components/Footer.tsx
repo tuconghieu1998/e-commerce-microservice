610c1bc7f2b53f01904e4abe88e76e41c51c9d57
hieu <tuconghieu1998@gmail.com>
2025-09-26 15:49:02 +0700

    UI: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,38 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:justify-between md:gap-0 bg-gray-800 p-8 rounded-lg ">
+            <div className="flex flex-col gap-4 items-center md:items-start">
+                <Link href="/" className="flex items-center">
+                    <Image src="/logo.png" alt="Shopping" width={36} height={36} className="w-6 h-6 md:w-9 md:h-9"></Image>
+                    <p className="text-md font-medium tracking-wider text-white"> SHOPPING</p>
+                </Link>
+                <p className="text-sm text-gray-400">2025 Shopping.</p>
+                <p className="text-sm text-gray-400">All rights reserved.</p>
+            </div>
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Links</p>
+                <Link href="/">Homepage</Link>
+                <Link href="/">Contact</Link>
+                <Link href="/">Terms of Service</Link>
+                <Link href="/">Privacy Policy</Link>
+            </div>
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Links</p>
+                <Link href="/">All Products</Link>
+                <Link href="/">New Arrials</Link>
+                <Link href="/">Best Sellers</Link>
+                <Link href="/">Sale</Link>
+            </div>
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Links</p>
+                <Link href="/">About</Link>
+                <Link href="/">Contact</Link>
+                <Link href="/">Blog</Link>
+                <Link href="/">Afiliate Program</Link>
+            </div>
         </div>
     );
 }
