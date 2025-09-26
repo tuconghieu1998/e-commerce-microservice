5e607ab23732ff863e346be1694329614d4ff133
tuconghieu1998 <tuconghieu1998@gmail.com>
2025-09-26 23:12:24 +0700

    UI: category tags

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { ProductType } from "@/types";
+
+const ProductCard = ({product}:{product:ProductType}) => {
+    return (
+        <div>
+            Product Card
+        </div>
+    );
+}
+
+export default ProductCard;
