ffcc69c9da82dd7ff174f8ea955efe8372dcfa06
hieu <tuconghieu1998@gmail.com>
2025-09-26 15:31:12 +0700

    UI: create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import { Bell, ShoppingCart, Home } from "lucide-react";
+
+const Navbar = () => {
+    return (
+        <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
+            <Link href="/" className="flex items-center">
+            <Image src="/logo.png" alt="Shopping" width={36} height={36} className="w-6 h-6 md:w-9 md:h-9"></Image>
+            <p className="text-md font-medium tracking-wider"> SHOPPING</p>
+            </Link>
+
+            <div className="flex items-center gap-6">
+                <SearchBar />
+                <Link href="/">
+                    <Home className="w-4 h-4 text-gray-600"></Home>
+                </Link>
+                <Bell className="w-4 h-4 text-gray-600"></Bell>
+                <ShoppingCart className="w-4 h-4 text-gray-600"></ShoppingCart>
+                <Link href="/login">Sign In</Link>    
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
