7cd46b63e8d7bdc946f6f91efc7a08d82523a685
hieu <tuconghieu1998@gmail.com>
2025-10-06 09:38:20 +0700

    order service

--- my-turborepo/apps/order-service/src/index.ts
+++ my-turborepo/apps/order-service/src/index.ts
@@ -1,27 +1,45 @@
-import Fastify, { FastifyInstance } from "fastify";
+import Fastify from "fastify";
+import Clerk from "@clerk/fastify";
+import { shouldBeUser } from "./middleware/authMiddleware.js";
+import { connectOrderDB } from "@repo/order-db";
+import { orderRoute } from "./routes/order.js";
+import { consumer, producer } from "./utils/kafka.js";
+import { runKafkaSubscriptions } from "./utils/subscriptions.js";
 
-const fastify: FastifyInstance = Fastify({})
+const fastify = Fastify();
+
+fastify.register(Clerk.clerkPlugin);
 
 fastify.get("/health", (request, reply) => {
   return reply.status(200).send({
-        status: "ok",
-        uptime: process.uptime(),
-        timestamp: Date.now()
-    });
-})
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
+fastify.get("/test", { preHandler: shouldBeUser }, (request, reply) => {
+  return reply.send({
+    message: "Order service is authenticated!",
+    userId: request.userId,
+  });
+});
+
+fastify.register(orderRoute);
 
 const start = async () => {
   try {
+    Promise.all([
+      await connectOrderDB(),
+      await producer.connect(),
+      await consumer.connect(),
+    ]);
+    await runKafkaSubscriptions();
     await fastify.listen({ port: 8001 });
-    console.log("Order service is running on port 8001")
-
-    const address = fastify.server.address()
-    const port = typeof address === 'string' ? address : address?.port
-
+    console.log("Order service is running on port 8001");
   } catch (err) {
-    console.log(err)
-    process.exit(1)
+    console.log(err);
+    process.exit(1);
   }
-}
-
-start()
+};
+start();
