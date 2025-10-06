6c0834e750f161f377301d29734ddd9df0cda96c
hieu <tuconghieu1998@gmail.com>
2025-10-06 09:50:57 +0700

    client page

--- my-turborepo/apps/client/src/components/Navbar.tsx
+++ my-turborepo/apps/client/src/components/Navbar.tsx
@@ -3,13 +3,21 @@ import Image from "next/image";
 import SearchBar from "./SearchBar";
 import { Bell, ShoppingCart, Home } from "lucide-react";
 import ShoppingCartIcon from "./ShoppingCartIcon";
+import {
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    SignUpButton,
+    UserButton,
+} from "@clerk/nextjs";
+import ProfileButton from "./ProfileButton";
 
 const Navbar = () => {
     return (
         <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
             <Link href="/" className="flex items-center">
-            <Image src="/logo.png" alt="Shopping" width={36} height={36} className="w-6 h-6 md:w-9 md:h-9"></Image>
-            <p className="text-md font-medium tracking-wider"> SHOPPING</p>
+                <Image src="/logo.png" alt="Shopping" width={36} height={36} className="w-6 h-6 md:w-9 md:h-9"></Image>
+                <p className="text-md font-medium tracking-wider"> SHOPPING</p>
             </Link>
 
             <div className="flex items-center gap-6">
@@ -19,7 +27,12 @@ const Navbar = () => {
                 </Link>
                 <Bell className="w-4 h-4 text-gray-600"></Bell>
                 <ShoppingCartIcon></ShoppingCartIcon>
-                <Link href="/login">Sign In</Link>    
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <ProfileButton />
+                </SignedIn>
             </div>
         </nav>
     );
